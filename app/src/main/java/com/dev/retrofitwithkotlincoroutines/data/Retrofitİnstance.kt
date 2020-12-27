f704270dc28914bdf6709893caec6e00ac50eda5
y.yusifov <ynyusifov@gmail.com>
2020-12-28 00:52:16 +0400

    Add HttpLoggingInterceptor

--- app/src/main/java/com/dev/retrofitwithkotlincoroutines/data/Retrofitİnstance.kt
+++ app/src/main/java/com/dev/retrofitwithkotlincoroutines/data/Retrofitİnstance.kt
@@ -2,15 +2,25 @@ package com.dev.retrofitwithkotlincoroutines.data
 
 
 import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitInstance {
     companion object {
         val BASE_URL: String = "https://jsonplaceholder.typicode.com/"
+        val interceptor = HttpLoggingInterceptor().apply {
+            this.level = HttpLoggingInterceptor.Level.HEADERS
+        }
+        val okClient = OkHttpClient.Builder().apply {
+            this.addInterceptor(interceptor)
+        }.build()
+
         fun getRetrofitInstance(): Retrofit {
             return Retrofit.Builder()
                 .baseUrl(BASE_URL)
+                .client(okClient)
                 .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
                 .build()
         }
